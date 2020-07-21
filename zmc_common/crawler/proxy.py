a94dfa3f61fd621c2f0a8d256443059f86b48011
Ubuntu <ubuntu@ip-172-31-6-7.cn-northwest-1.compute.internal>
2020-07-21 18:48:15 +0800

    update

--- zmc_common/crawler/proxy.py
+++ zmc_common/crawler/proxy.py
@@ -17,19 +17,26 @@ class ProxyMiddleware(object):
         port = proxy['prot']
         proxy_url = f'http://{host}:{port}'
         # 打印结果出来观察
-        logger.debug("this is request ip:" + proxy_url)
+        logger.info("this is request ip url:" + proxy_url)
         # 设置request的proxy属性的内容为代理ip
         request.meta['proxy'] = proxy_url
+        request.meta['proxy_url'] = proxy_url
+        request.meta['proxy_host'] = host
+        request.meta['proxy_port'] = port
 
-    # # Downloader Middleware的核心方法，只有实现了其中一个或多个方法才算自定义了一个Downloader Middleware
-    # def process_response(self, request, response, spider):
-    #     # 请求失败不等于200
-    #     if response.status != 200:
-    #         # 重新选择一个代理ip
-    #         proxy = random.choice(self.proxyList).strip()
-    #         print("this is response ip:" + proxy)
-    #         # 设置新的代理ip内容
-    #         request.mete['proxy'] = proxy
-    #         return request
-    #     return response
+    # Downloader Middleware的核心方法，只有实现了其中一个或多个方法才算自定义了一个Downloader Middleware
+    def process_response(self, request, response, spider):
+        # 请求失败不等于200
+        if response.status != 200:
+            host =response.meta['proxy_host']
+            port =response.meta['proxy_port']
+            logger.info(f"proxy failed {host}:{port}")
+            # 重新选择一个代理ip
+            # proxy = random.choice(self.proxyList).strip()
+            # print("this is response ip:" + proxy)
+            # # 设置新的代理ip内容
+            # request.mete['proxy'] = proxy
+            # return request
+            proxy_list.remove_proxy(host,port)
+        return response
 
