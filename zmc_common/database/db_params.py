e4f95a652d5009ca93da0102309674b0bf2b9d35
Ubuntu <ubuntu@ip-172-31-6-7.cn-northwest-1.compute.internal>
2020-07-21 11:58:15 +0800

    update

--- zmc_common/database/db_params.py
+++ zmc_common/database/db_params.py
@@ -1,13 +1,19 @@
 
 
 
-def get_zmc_postgres_customer(custom_id):
+def __zmc_postgres_db():
     return {
         "host": "172.22.57.108",
         "port": 5432,
         "user": "postgres",
         "password": "postgres",
-        "database": f"z_{str(custom_id).rjust(5,'0')}",
         'charset': 'utf8',
         'protocol':'postgres'
     }
+
+
+
+def zmc_postgres_customer_params(custom_id):
+    params = __zmc_postgres_db()
+    params['database'] =f"z_{str(custom_id).rjust(5,'0')}"
+    return params
