406623263663a598236d9f92ecf981272d634649
wangcenhan <wangcenhan@xiaoniuhy.com>
2020-11-09 12:15:07 +0800

    update

--- python_common/database/client/conf.py
+++ python_common/database/client/conf.py
@@ -1,4 +1,14 @@
 
+mysql_adsense_pizarr ={
+    'protocol':'mysql',
+    'user':'ads_read',
+    'password':'3UmA#kkSD0th8VM',
+    'host':'rr-uf6z09z4vxcr8li31.mysql.rds.aliyuncs.com',
+    'port':3306,
+    'database':'adsense_pizarr'
+}
+
+
 clickhouse_xn_adp ={
     'protocol':'clickhouse',
     'user':'chaixiaohui',
@@ -27,4 +37,5 @@ db_params = {
     'default':clickhouse_xn_adp,
     'ch_xn_adp':clickhouse_xn_adp,
     'ch_xn_adp_knowledge':clickhouse_xn_adp_knowledge,
+    'mysql_adsense_pizarr':mysql_adsense_pizarr,
 }
