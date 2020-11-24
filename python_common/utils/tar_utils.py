1839f9ae355603cab962f0202cba6b83bdd89f05
wangcenhan <wangcenhan@xiaoniuhy.com>
2020-11-24 18:18:16 +0800

    update

---
+++ python_common/utils/tar_utils.py
@@ -0,0 +1,16 @@
+import os, tarfile
+
+def untar(fname, dirs):
+    """
+    解压tar.gz文件
+    :param fname: 压缩文件名
+    :param dirs: 解压后的存放路径
+    :return: bool
+    """
+    try:
+        t = tarfile.open(fname)
+        t.extractall(path = dirs)
+        return True
+    except Exception as e:
+        print(e)
+        return False
