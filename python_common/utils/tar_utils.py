7d468d0d35c423ac7c0a8794f90c197ddb821b87
wangcenhan <wangcenhan@xiaoniuhy.com>
2020-11-24 20:23:12 +0800

    update

--- python_common/utils/tar_utils.py
+++ python_common/utils/tar_utils.py
@@ -1,5 +1,27 @@
 import os, tarfile
 
+
+def tar(output_filename, source_dir):
+    """
+    一次性打包目录为tar.gz
+    :param output_filename: 压缩文件名
+    :param source_dir: 需要打包的目录
+    :return: bool
+    """
+    try:
+        with tarfile.open(output_filename, "w:gz") as tar:
+            if isinstance(source_dir,list):
+                for i in source_dir:
+                    tar.add(i, arcname=os.path.basename(i))
+            else:
+                print(os.path.basename(source_dir))
+                tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
 def untar(fname, dirs):
     """
     解压tar.gz文件
---
+++ test/utils/test_tar.py
@@ -0,0 +1,4 @@
+from python_common.utils.tar_utils import tar
+
+
+tar("test.tar.gz",'test/utils')
