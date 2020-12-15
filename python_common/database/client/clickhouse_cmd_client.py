d0d72dfd28a452e04f35019f5203190398c21c6f
wangcenhan <wangcenhan@xiaoniuhy.com>
2020-12-15 21:26:36 +0800

    update

--- python_common/database/client/clickhouse_cmd_client.py
+++ python_common/database/client/clickhouse_cmd_client.py
@@ -20,6 +20,7 @@ class ClickhouseCmdClient(BaseClient):
         self._base_dir = base_dir
         if not os.path.exists( self._base_dir):
             os.makedirs( self._base_dir)
+        self._cache = []
     
     def _build_read_cmd(self,query_path,data_path):
         cmd_list = [f'cat {query_path} | clickhouse-client']
@@ -96,24 +97,41 @@ class ClickhouseCmdClient(BaseClient):
     def exec_sql(self, sql):
         raise Exception('un imp!')
 
-    @func_elapsed
-    def to_sql(self,df,table,index=False,if_exists='append'):
+    def write(self,records,table):
         dt = datetime.now()
-        data_path = self._base_dir + "/write_sql_data_{ts:%y%m%d_%H%M%S_%f}.csv".format(ts=dt)
-        query_path = self._base_dir + "/write_sql_query_{ts:%y%m%d_%H%M%S_%f}.txt".format(ts=dt)
-        if isinstance(df,pd.DataFrame):
-            records = df.to_dict("records")
+        data_path = self._base_dir + "/write_sql_data_{ts:%y%m%d_%H%M%S_%f}.json".format(ts=dt)
+        query_path = self._base_dir + "/write_sql_query_{ts:%y%m%d_%H%M%S_%f}.sql".format(ts=dt)
+        result = None
+        try:
             with open(data_path,'w') as f:
                 for i in records:
                     f.write(f'{json.dumps(i)}\n')
-        cmd = self._build_write_cmd(query_path,data_path,table)
-        result = run_cli(cmd)
+            cmd = self._build_write_cmd(query_path,data_path,table)
+            result = run_cli(cmd)
+        finally:
+            if os.path.exists(data_path):
+                os.remove(data_path)
 
-        if os.path.exists(data_path):
-            os.remove(data_path)
+            if os.path.exists(query_path):
+                os.remove(query_path)
 
-        if os.path.exists(query_path):
-            os.remove(query_path)
+        return result
+
+    def cached_to_sql(self,row,table,index=False,if_exists='append',cached_len = 8192, flush=False):
+        if len(self._cache) > cached_len or flush:
+            cache =  self._cache.copy()
+            self._cache.clear()
+            return self.write(cache,table)
+        else:
+            self._cache.append(row)
+            return None
+
+
+    @func_elapsed
+    def to_sql(self,df,table,index=False,if_exists='append'):
+        if isinstance(df,pd.DataFrame):
+            records = df.to_dict("records")
+            result = self.write(records,table)
 
         return result
 
--- test/test_datetime.py
+++ test/test_datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime,timedelta
-from zmc_common.utils.datetime_utils import infer_datetime
-from zmc_common.utils.datetime_utils import datetime_format
+from python_common.utils.datetime_utils import infer_datetime
+from python_common.utils.datetime_utils import datetime_format
 # for i  in range(7):
 #     print(datetime_format(datetime.now() + timedelta(days=i)))
 
@@ -8,3 +8,6 @@ dt = infer_datetime('05月20日')
 print(dt)
 dt = infer_datetime('05月20日 周三 14:00')
 print(dt)
+
+
+print(datetime.now().minute)
