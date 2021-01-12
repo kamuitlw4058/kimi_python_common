69503b5a80d955f908b2b204d05a3aef5916b6f0
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-01-12 15:07:43 +0800

    update

--- test/database/test_redis.py
+++ test/database/test_redis.py
@@ -1,8 +1,16 @@
 from python_common.kv.redis import RedisClient
+import time
+
 redis = RedisClient(host='192.168.3.64')
 key1 = 'key1'
 v = redis.get(key1)
 print(v)
 redis.set(key1,'1')
+v = redis.get(key1)
+print(v)
+redis.set(key1,'2',3)
+v = redis.get(key1)
+print(v)
+time.sleep(5)
 v = redis.get(key1)
 print(v)
