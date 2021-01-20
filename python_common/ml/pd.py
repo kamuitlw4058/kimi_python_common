d6e8f1e91b6b5a1bc756d68fafaf4a5b7cb3bf43
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-01-20 16:08:39 +0800

    update

---
+++ python_common/ml/pd.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+pd.set_option('display.max_colwidth', 30)
+pd.set_option('display.width', 300)  # 150，设置打印宽度
+pd.set_option('display.max_rows', 30) # 打印最大行数
+pd.set_option('display.max_columns', 40) # 打印最大列数
+
+pd
