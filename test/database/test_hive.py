967dd836d75d44ba3163835c76e8eaa43479591e
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-01-20 15:31:31 +0800

    update

--- test/database/test_hive.py
+++ test/database/test_hive.py
@@ -5,7 +5,8 @@
 # print(df)
 
 from impala.dbapi import connect
-conn = connect(host='192.168.66.115', port=21050)
+conn = connect(host='192.168.66.115', port=10000,auth_mechanism='PLAIN', user='suanfa',
+                       password='suanfa123', database='dc_ods_voice')
 cur = conn.cursor()
 cur.execute('SHOW DATABASES')
 data_list=cur.fetchall()
