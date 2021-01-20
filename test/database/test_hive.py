db7f6e2c838a62f8bfcf3b80a132c15ba0da551d
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-01-20 15:18:16 +0800

    update

--- test/database/test_hive.py
+++ test/database/test_hive.py
@@ -1,5 +1,12 @@
-from python_common.database.client.clients import clients
+# from python_common.database.client.clients import clients
 
-client = clients.client('hive_xn_bigdata')
-df = client.read_sql('show tables')
-print(df)
+# client = clients.client('hive_xn_bigdata')
+# df = client.read_sql('show tables')
+# print(df)
+
+from impala.dbapi import connect
+conn = connect(host='192.168.66.115', port=10000)
+cur = conn.cursor()
+cur.execute('show tables')
+data_list=cur.fetchall()
+    for data in data_list:
