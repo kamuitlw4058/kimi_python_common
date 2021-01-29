3cd263b219045fb579254d4feb962051e79c4439
wangcenhan <wangcenhan@xiaoniuhy.com>
2021-01-29 11:34:31 +0800

    update

---
+++ python_common/io/scan_datafiles.py
@@ -0,0 +1,103 @@
+import os
+from types import FunctionType, MethodType
+from python_common.utils.datetime_utils import  str_to_datetime
+
+def false_filter(filepath,filename):
+    return False
+
+def true_filter(filepath,filename):
+    return True
+
+class ScanDataFiles():
+    def __init__(self,job_name,data_path_base,file_filters=None,dir_filters=None, ckpt_dir_base='data/scan_data_ckpt'):
+        self._name = job_name
+        self._files = []
+        self._ckpt_dir_base = ckpt_dir_base
+        self._ckpt_dir = f'{self._ckpt_dir_base}/{job_name}'
+        if not os.path.exists(self._ckpt_dir):
+            os.makedirs(self._ckpt_dir)
+        self._data_path_base = data_path_base
+        self._dir_filters = dir_filters
+        self._file_filters = file_filters
+
+    def init_filters(self,filters,default_filter,default_filters=None):
+        apply_filter = None
+        if filters is None:
+            filters = default_filters
+            if filters is None or (isinstance( filters,list) and len(filters) == 0):
+                filters = default_filter
+
+        if isinstance( filters,list):
+            apply_filter = filters[0]
+            #print(f'list filters get apply_filter:{apply_filter}')
+            filters = filters[1:]
+        elif  isinstance( filters,FunctionType):
+            apply_filter = filters
+
+        return apply_filter,filters
+
+
+
+
+    def scan(self,base_dir =None,data_dir=None,file_filters=None,dir_filters=None):
+        if base_dir is None:
+            base_dir = self._data_path_base
+        if data_dir is None:
+            data_dir = ''
+        
+        dir_path = os.path.join(base_dir,data_dir)
+
+        dir_filter,dir_next_filters = self.init_filters(dir_filters,true_filter,default_filters =self._dir_filters)
+        file_filter,file_next_filters = self.init_filters(file_filters,true_filter,default_filters = self._file_filters)
+
+        for filename in os.listdir(dir_path):
+            filepath = os.path.join(dir_path, filename)
+            if os.path.isdir(filepath) and dir_filter(filepath,filename):
+                print(f"will check dir:{filepath}")
+                self.scan(base_dir,os.path.join(data_dir,filename),file_filters=file_next_filters,dir_filters=dir_next_filters)
+            if os.path.isfile(filepath) and file_filter(filepath,filename):
+                print(f'will process file:{filepath}')
+                self._files.append((base_dir,data_dir,filename))
+
+
+    def process_files(self):
+        print(self._files)
+
+
+def level2_file_filter(filepath,filename):
+    #print(filename)
+    if filename.startswith("part"):
+        return True
+    else:
+        return False
+
+file_filters = [
+    false_filter,
+    level2_file_filter,
+    level2_file_filter
+]
+
+
+
+
+def level1_dir_filter(filepath,filename):
+    try:
+        dir_date = str_to_datetime(dirpath)
+        if  dir_date.date() >=  (datetime.now() - timedelta(hours=5)).date():
+            return True
+    except Exception as e:
+        pass
+
+    return False
+
+
+
+dir_filters = [
+    level1_dir_filter,
+    true_filter
+]
+
+
+s = ScanDataFiles('test','data/test_datasource',file_filters=false_filter,dir_filters=dir_filters)
+s.scan()
+s.process_files()
--- python_common/ml/pd.py
+++ python_common/ml/pd.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-pd.set_option('display.max_colwidth', 30)
+pd.set_option('display.max_colwidth', 20)
 pd.set_option('display.width', 300)  # 150，设置打印宽度
 pd.set_option('display.max_rows', 30) # 打印最大行数
 pd.set_option('display.max_columns', 40) # 打印最大列数
 
 pd
+
+
+
+
